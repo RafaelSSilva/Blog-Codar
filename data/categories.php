9285d09523f5e57072912a48716b33bbbdf74b98
Rafael Silva <rafael.ssilva03@gmail.com>
2021-04-24 13:46:49 -0300

    Setup do projeto.

---
+++ data/categories.php
@@ -0,0 +1,10 @@
+<?php
+  $categories = [
+    'PHP',
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Bootstrap',
+    'Java',
+    'C#'
+  ];
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    require_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- TEMPLATE DO PROJETO -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+    
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    
+    <title>Blog Codar</title>
+</head>
+<body>
+    Olá Mundo!
+</body>
+</html>
